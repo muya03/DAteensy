ab984e4b8ec1d8ae56bbc1c5662d5a01e44b552d
vik0t0r <al404303@uji.es>
2022-04-04 16:50:33 +0200

    inital commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
