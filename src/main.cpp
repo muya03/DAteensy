79d748f572f52eb4bf5f0e260d987617e443ef60
vik0t0r <al404303@uji.es>
2022-05-20 17:47:49 +0200

    added screen

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,11 @@
 #include "lib/persistence/persistance.h"
 #include "lib/SD/SDstore.h"
 
+#include "lib/SteeringWheel/SteeringWheel.h"
+
 #include "settings.h"
 
 OBD2sensordata OBD2db = {0};
-
 
 
 void setup() {
@@ -15,6 +16,8 @@ void setup() {
     #ifdef DEBUG
     Serial.begin(115200); 
     #endif
+
+    initScreen(ScreenUART);
 
     initOBD2(OBD2db);
     initSD();
@@ -28,31 +31,37 @@ uint32_t elapsed_100ms = 0;
 void loop() {
     // execute always
 
-    //OBD2events();
+    OBD2events();
 
     // update screen
-
+    sendRPM(OBD2RPM(OBD2db));
     // update rpm leds
 
     // execute each 100ms
     if (millis() - elapsed_100ms > 100){
         // print data to sd
-
+        String to_save = "";
+        to_save += OBD2toCSV(OBD2db);
+        saveLine(to_save);
         elapsed_100ms = millis();
     }
 
     // execute each second
     if (millis() - elapsed_second > 1000){
-        // print data to log file
-        saveLine(OBD2toCSV(OBD2db));
+        // send data over radio
+
+        
         elapsed_second = millis();
     }
 
     // execute each minute
     if (millis() - elapsed_minute > 60* 1000){
 
+        // increase time alive counter
         increaseTimeCounter(EEPROM_base_address);
         elapsed_minute = millis();
+        // increase engine on time??
+
     }
 
 }
